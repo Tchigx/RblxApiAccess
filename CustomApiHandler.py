11215bf3a9b48c19ae7eb6b0d99a95e95e22a254
TChigx <tawananyashachigombe@gmail.com>
2024-04-21 18:26:16 +0200

    mmm

--- CustomApiHandler.py
+++ CustomApiHandler.py
@@ -7,22 +7,22 @@ app = Flask(__name__)
 @app.route('/GetAssetBySearch', methods=['GET'])
 def handle_endpoint():
     # Access query parameters
-    param1 = request.args.get('param1', default=None, type=str)
-    param2 = request.args.get('param2', default=None, type=str)
+    param1 = request.args.get('TypeAsset', default=None, type=str)
+    param2 = request.args.get('CreatorID', default=None, type=str)
    # param3 = request.args.get('param2', default=None, type=str)
 
-    #FinalStr=""
-    #if param1=="Decal":
-    #    FinalStr=f"https://search.roblox.com/catalog/json?CreatorID={param2}&SortType=3&PageNumber=1&Category=8"
-    #elif param1=="Audio":
-    #    FinalStr=f"https://search.roblox.com/catalog/json?CreatorID={param2}&SortType=3&PageNumber=1&Category=9"
+    FinalStr=""
+    if param1=="Decal":
+        FinalStr=f"https://search.roblox.com/catalog/json?CreatorID={param2}&SortType=3&PageNumber=1&Category=8"
+    elif param1=="Audio":
+        FinalStr=f"https://search.roblox.com/catalog/json?CreatorID={param2}&SortType=3&PageNumber=1&Category=9"
     
 
-    #page=requests.get(FinalStr)
-   # page=page.json()
-
+    page=requests.get(FinalStr)
+    page=page.json()
+    print(page)
     # Use the query parameters in your response or processing
-    return jsonify({'param1': param1, 'param2': param2}), 200
+    return jsonify(page), 200
     
 
 @app.errorhandler(404)
